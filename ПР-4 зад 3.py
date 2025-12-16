b967cb808bdd111ae48e4af14a1bc934e94586e7
sasasasa7374-dotcom <sasasasa7374@gmail.com>
2025-12-16 12:35:59 +0700

    Update ПР-4 зад 3.py

--- ПР-4 зад 3.py
+++ ПР-4 зад 3.py
@@ -1,18 +1,18 @@
 class BankAccount:
-    def init(self, initial_balance=0):
+    def _init_(self, initial_balance=0):
         self.__balance = initial_balance
 
     def deposit(self, amount):
         if amount > 0:
             self.__balance += amount
         else:
-            raise ValueError("Сумма депозита должна быть положительной.")
+            raise Error("Сумма депозита должна быть положительной.")
 
     def withdraw(self, amount):
         if amount > self.__balance:
-            raise ValueError("Недостаток средств")
+            raise Error("Недостаток средств")
         if amount <= 0:
-            raise ValueError("Сумма вывода должна быть положительной")
+            raise Error("Сумма вывода должна быть положительной")
         self.__balance -= amount
 
     def get_balance(self):
